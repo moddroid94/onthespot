c576b68b3ac3db9af20a0cb923f2f124558710c3
Justin Donofrio <justin026@protonmail.com>
2024-08-25 20:35:33 -0400

    Starting without Terminal on Windows
    https://github.com/casualsnek/onthespot/pull/132/files

--- build_winC2.bat
+++ build_winC2.bat
@@ -1,16 +1,17 @@
 @echo off
 echo  =^> Installing 'pyinstaller' via pip...
-python -m pip install --upgrade pip
-pip install wheel pillow pyinstaller
+pip install --upgrade pip
+pip install wheel
+pip install pyinstaller
 echo  =^> Installing dependencies pip...
 pip install winsdk
 pip install -r requirements.txt
 if exist ffbin_win\ffmpeg.exe (
     echo =^> Found 'ffbin_win' directory and ffmpeg binary.. Using ffmpeg binary append mode
-    pyinstaller --onefile --noconfirm --add-data="src/onthespot/gui/qtui/*.ui;onthespot/gui/qtui" --add-data="src/onthespot/resources/*.png;onthespot/resources" --add-binary="ffbin_win/*.exe;onthespot/bin/ffmpeg" --paths="src/onthespot" --name="onthespot_win_ffm" --icon="src/onthespot/resources/icon.png" src\portable.py
+    pyinstaller --onefile --noconsole --noconfirm --add-data="src/onthespot/gui/qtui/*.ui;onthespot/gui/qtui" --add-data="src/onthespot/resources/*.png;onthespot/resources" --add-binary="ffbin_win/*.exe;onthespot/bin/ffmpeg" --paths="src/onthespot" --name="onthespot_win_ffm" --icon="src/onthespot/resources/icon.png" src\portable.py
 ) else (
     echo  =^> Building to use ffmpeg binary from system...
-    pyinstaller --onefile --noconfirm --add-data="src/onthespot/gui/qtui/*.ui;onthespot/gui/qtui" --add-data="src/onthespot/resources/*.png;onthespot/resources" --paths="src/onthespot" --name="onthespot_win" --icon="src/onthespot/resources/icon.png" src\portable.py
+    pyinstaller --onefile --noconsole --noconfirm --add-data="src/onthespot/gui/qtui/*.ui;onthespot/gui/qtui" --add-data="src/onthespot/resources/*.png;onthespot/resources" --paths="src/onthespot" --name="onthespot_win" --icon="src/onthespot/resources/icon.png" src\portable.py
 )
 echo  =^> Cleaning..
 if exist onthespot_win.spec (
