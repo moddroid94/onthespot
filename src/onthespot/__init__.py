7967590808a64bfd197590d58b9388249040494e
Justin <153443117+justin025@users.noreply.github.com>
2024-09-24 21:13:30 -0400

    Fix float error bug on windows

--- src/onthespot/__init__.py
+++ src/onthespot/__init__.py
@@ -18,7 +18,7 @@ def main():
     logger.info(f'OnTheSpot Version: {version}')
 
     # Migration (<v0.7)
-    if int(version.replace('v', '').replace('.', '')) > int(config.get("version").replace('v', '').replace('.', '')):
+    if int(version.replace('v', '').replace('.', '')) > int(str(config.get("version")).replace('v', '').replace('.', '')):
         if " " not in config.get("metadata_seperator"):
             config.set_("metadata_seperator", config.get("metadata_seperator")+" ")
         config.set_("download_play_btn", False)
