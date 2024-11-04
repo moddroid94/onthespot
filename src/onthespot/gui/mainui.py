b2789205bc716475a9eca18d70768dd7a04bd328
Justin Donofrio <justin026@protonmail.com>
2024-11-04 09:33:14 -0500

    Stop main ui worker from freezing

--- src/onthespot/gui/mainui.py
+++ src/onthespot/gui/mainui.py
@@ -30,14 +30,19 @@ class QueueWorker(QThread):
     def run(self):
         while True:
             if pending:
-                item_id = next(iter(pending))  # Get the first key  
-                item = pending.pop(item_id)
-                token = get_account_token()
-                item_metadata = globals()[f"{item['item_service']}_get_{item['item_type']}_metadata"](token, item['item_id'])
-                self.add_item_to_download_list.emit(item, item_metadata)
-                continue
+                try:
+                    item_id = next(iter(pending)) 
+                    item = pending.pop(item_id)
+                    token = get_account_token()
+                    item_metadata = globals()[f"{item['item_service']}_get_{item['item_type']}_metadata"](token, item['item_id'])
+                    self.add_item_to_download_list.emit(item, item_metadata)
+                    continue
+                except Exception as e:
+                    logger.error(f"Unknown Exception for {item}: {str(e)}")
+                    pending[item_id] = item
             else:
                 time.sleep(4)
+
 
 class MainWindow(QMainWindow):
 
